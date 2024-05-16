905b46a3ccaf50b7feb1d5b69ea7f562d0051381
manhG (Sky) <nguyenmanhict@gmail.com>
2024-05-17 01:12:06 +0700

    first commit

--- src/api/http-common.js
+++ src/api/http-common.js
@@ -2,7 +2,7 @@ var axios = require('axios');
 
 var configData = {
     baseURL: process.env.NODE_ENV === 'production'
-        ? process.env.API_URL : process.env.DEV_URL,
+        ? process.env.API_URL : process.env.API_URL,
     headers: {
         "content-type": "application/json",
         "token": "eyJ0eXAiOiJqd3QiLCJhbGciOiJIUzI1NiJ9.eyJxxx"
